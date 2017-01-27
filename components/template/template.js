ba344c49cc96e9839de3bab806b5de811878bbfd
Jeremy <jeremydog1@gmail.com>
2017-01-27 20:44:29 +1300

    Refactor to use dependency injection for template using new templateService

--- components/template/template.js
+++ components/template/template.js
@@ -1,41 +1,23 @@
-app1.controller('templateController', function ($scope) {
+app1.controller('templateController', function ($scope, templateService) {
 
-	$scope.template = {
+	//Template stuff
+	$scope.template = templateService;
 
-		tName: '',
-		aspects: [],
+	$scope.createAspect = templateService.createAspect;
+
+	$scope.addCommentToAspect = function() {
+		templateService.aspects[$scope.currentAspect].addComment();
+	};
+	$scope.removeCommentFromCurrentAspect = function(index) {
+		templateService.aspects[$scope.currentAspect].removeComment(index);
 	};
 
-	/*
-	 * aspectName - String: Name of aspect eg. Attendence
-	 * comments - [[adjective, comment]]:  2D array of comments with an adjective attached.
-	 *		eg. ['Poor', 'Numerous absences have contributed to a lack of work and engagement with the subject.']
-	 */
-	$scope.createAspect = function(name, comments) {
-		var aspect = function(name, comments) {
-			this.aName = name;
-			this.comments = comments;
-
-			this.addComment = function() {
-				this.comments.push([[''], ['']]);
-			};
-
-			this.removeComment = function(index) {
-				this.comments.splice(index, 1);
-			};
-		}
-		return(new aspect(name, comments));
+	$scope.loadExample = function() {
+		templateService.createAspect('Attendence', [['Good', 'Good attendance'], ['Poor', 'Poor attendance']]);
+		templateService.createAspect('Homework', [['Good', 'Well done with homework'], ['Poor', 'Poor homework']]);
 	};
 
-	$scope.create = function() {
-		$scope.template.aspects.push($scope.createAspect('Attendence', [['Good', 'Good attendance'], ['Poor', 'Poor attendance']]));
-		$scope.template.aspects.push($scope.createAspect('Homework', [['Good', 'Well done with homework'], ['Poor', 'Poor homework']]));
-
-		for (a in $scope.template.aspects){
-			console.log($scope.template.aspects[a].aName);
-		}
-	};
-
+	// Remember which aspect tab is selected.
 	$scope.currentAspect = 1;
 	$scope.changeAspect = function(index) {
 			$scope.currentAspect = index;
@@ -47,11 +29,14 @@ app1.controller('templateController', function ($scope) {
 	$scope.toggleAddingAspect = function() {
 		$scope.addingAspect = !$scope.addingAspect;
 	};
+	
 	$scope.addAspect = function() {
 		const blankComment = [['', '']];
 
 		if ($scope.newAspectName != '') {
-			$scope.template.aspects.push($scope.createAspect($scope.newAspectName, blankComment));
+			templateService.createAspect($scope.newAspectName, blankComment);
+
+			//clean up
 			$scope.newAspectName = '';
 			$scope.currentAspect = $scope.template.aspects.length - 1;
 			$scope.toggleAddingAspect();
@@ -59,13 +44,6 @@ app1.controller('templateController', function ($scope) {
 		else {
 			$scope.toggleAddingAspect();
 		}
-	};
-	$scope.addCommentToAspect = function(aspectIndex) {
-		$scope.template.aspects[aspectIndex].addComment();
-	};
-
-	$scope.removeCommentFromCurrentAspect = function(index) {
-		$scope.template.aspects[$scope.currentAspect].removeComment(index);
 	};
 
 	// Greeting message
