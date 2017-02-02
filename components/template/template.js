4e71917632b743ae9cc2dca6a9c3e72ae46c3ed9
Jeremy <jeremydog1@gmail.com>
2017-02-02 16:38:00 +1300

    Improved default template

--- components/template/template.js
+++ components/template/template.js
@@ -13,8 +13,35 @@ app1.controller('templateController', function ($scope, templateService) {
 	};
 
 	$scope.loadExample = function() {
-		templateService.createAspect('Attendence', [['Good', 'Good attendance'], ['Poor', 'Poor attendance']]);
-		templateService.createAspect('Homework', [['Good', 'Well done with homework'], ['Poor', 'Poor homework']]);
+		templateService.createAspect('General comment', [
+			['Excellent', 'x is a focused student who has an obvious passion for the subject.'],
+			['Good', 'x is a pleasant young man who has approached Art with an open mind and a willingness to learn new things.'],
+			['Has potential', 'x is a capable student who has achieved good grades thus far, but I believe he is capable of better.'],
+			['Absent a lot', 'Numerous absences have contributed to a lack of work and engagement with the subject.'],
+		]);
+
+		templateService.createAspect('Homework', [
+			['Average', 'More focused effort and homework to ensure work is completed will help x to achieve better.'],
+			['Poor', 'x has not managed to complete any homework at all this term, which has been a detriment to x\'s grades.'],
+			['Improved', 'x has made a much greater effort this term to complete his homework, which has lead to a marked improvement in his grades.']
+		]);
+
+		templateService.createAspect('Grades', [
+			['Excellence', 'x should be proud of the grades he has achieved this term.'],
+			['High merit', 'x has achieved well this term, some extra attention to detail will enable him to turn his merit grades into excellence'],
+			['Merit', 'x has performed well this term in his assessments achieving a grade of merit, well done.'],
+			['Low merit', 'x has managed to improve his work ethic this term and improve his achieved grade to merit.'],
+			['High achieved', 'x was close to receiving a merit grade, but a lack of attention to detail has held it to the high end of achieved.'],
+			['Achieved', '---- Achieved grade comment ----'],
+			['Low achieved', '---- Low achieved grade comment ----'],
+			['High not-achieved', '---- High not-achieved comment ----'],
+			['Not Achieved', '---- Not-achieved comment ----']
+		]);
+
+		templateService.createAspect('Work', [
+			['Good', 'Well done with homework'],
+			['Poor', 'Poor homework']
+		]);
 	};
 
 	// Remember which aspect tab is selected.
@@ -29,7 +56,7 @@ app1.controller('templateController', function ($scope, templateService) {
 	$scope.toggleAddingAspect = function() {
 		$scope.addingAspect = !$scope.addingAspect;
 	};
-	
+
 	$scope.addAspect = function() {
 		const blankComment = [['', '']];
 
@@ -50,5 +77,6 @@ app1.controller('templateController', function ($scope, templateService) {
 	$scope.showOverlay = true;
 	$scope.toggleOverlay = function() {
 		$scope.showOverlay = !$scope.showOverlay;
+		$scope.loadExample(); //TODO: TEMPORARY
 	};
 });
