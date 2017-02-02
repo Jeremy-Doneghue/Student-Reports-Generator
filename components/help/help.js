0114bb1d5e869e12033faaed365d078c51763e05
Jeremy <jeremydog1@gmail.com>
2017-02-02 16:45:48 +1300

    Update help file. Create listing for report builder page.

--- components/help/help.js
+++ components/help/help.js
@@ -8,8 +8,12 @@ app1.controller('helpController', function($scope) {
 
 	const createTemplateHelp = [
 		['Aspects', 'These are the topics you want to comment on about your students. For example, homework or attitude in class. When it comes to creating the reports, you don\'t necesarily need to use each aspect on every student. You can just pick the most relevant ones.'],
-		['Test', '123']
+		['Pronouns?', 'You can create your template with whatever pronouns you like. The algorithm will automatically swap them to be appropriate to each student.']
 	];
 
-	$scope.helpComments = [importStudentsHelp, createTemplateHelp];
+	const reportBuilderHelp = [
+		['Placeholder', 'Lorem ipsum dolor sit amet. Consectetur adipiscing elit, fusce ut tellus nec.']
+	];
+
+	$scope.helpComments = [importStudentsHelp, createTemplateHelp, reportBuilderHelp];
 });
