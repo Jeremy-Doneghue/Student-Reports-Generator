8b35002857551e5d407bb01f1a462ed308857d06
Jeremy <jeremydog1@gmail.com>
2017-02-02 16:42:18 +1300

    Add description for report builder page. Remove redundant field from views object.

--- components/viewController/viewController.js
+++ components/viewController/viewController.js
@@ -3,23 +3,22 @@ app1.controller('viewController', function ($scope) {
 
 	$scope.views = [
 		{
-			src: 'fragments/import.html',
 			jTitle: 'Import class list',
 			jDescription: 'Import the names of your students by pasting in a list or entering them using the form.'
 		},
 		{
-			src: 'fragments/template.html',
 			jTitle: 'Create template',
 			jDescription: 'Create a template that you will use to quickly generate reports based on the criteria you set.'
+		},
+		{
+			jTitle: 'Build report',
+			jDescription: 'Generate reports for your students using the template.'
 		}
 	];
 
 	$scope.currentView = 0;
 
 	$scope.switchView = function () {
-		if ($scope.currentView == 0) {
-			$scope.currentView = 1;
-		}
-		else $scope.currentView = 0;
+		($scope.currentView == 2) ? $scope.currentView = 0 : $scope.currentView++;
 	}
 });
